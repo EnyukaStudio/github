47f99cc8e7883e6fdd21ff876746e17914f8651d
Mateusz Kozak <mateusz@mkozak.pl>
2020-04-19 19:43:12 +0200

    Revert "update faraday version and fix deprecated class issue"
    This reverts commit 52cb0f6c199e66a52f5c9b93e32c8178a319b01c.

--- lib/github_api/response/follow_redirects.rb
+++ lib/github_api/response/follow_redirects.rb
@@ -8,7 +8,7 @@ require 'set'
 
 module Github
   # Public: Exception thrown when the maximum amount of requests is exceeded.
-  class RedirectLimitReached < Faraday::ClientError
+  class RedirectLimitReached < Faraday::Error::ClientError
     attr_reader :response
 
     def initialize(response)
